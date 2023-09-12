5504f62d6c1f4a63e032ead007263678aae4adf7
lramir14 <73917621+lramir14@users.noreply.github.com>
2023-09-12 07:36:10 -0600

    changes in script. Test 1

---
+++ new_script.R
@@ -0,0 +1,17 @@
+# Set-up your script ------------------------------------------------------
+
+# install.packages(c("tidyverse", "gapminder", "pacman")) # uncomment if already installed
+pacman::p_load(tidyverse, gapminder)
+library(dplyr)
+# Load your Data into R ---------------------------------------------------
+
+data(gapminder)
+head(gapminder)
+
+# Clean your Data ---------------------------------------------------------
+
+gapminder_clean <- gapminder %>% 
+  rename(life_exp = lifeExp, gdp_per_cap = gdpPercap) %>% 
+  mutate(gdp = pop * gdp_per_cap)
+
+data_sub<- subset(gapminder_clean, continent == "Americas")
